e1104b81f57e2eb3c29f8509c957fb978b917e39
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2022-09-07 14:42:01 +0200

    setup: Create react project with Vite & Add TailwindCSS

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p className='text-5xl font-bold underline'>Hello world!</p>;
+}
+
+export default App;
---
+++ client/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
